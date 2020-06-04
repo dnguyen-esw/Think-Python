9133f52f5de10781eb3ed0fec35b0b4c33d0cb41
ng-hoangduc <hoangduc104@gmail.com>
2020-06-04 23:29:59 +0700

    test1.py

---
+++ test1.py
@@ -0,0 +1,11 @@
+s=10
+t=43.5
+time_per_mile=t/s
+print(time_per_mile)
+avr=(10/1.61)/(43.5/60)
+print("average speed",avr)
+print(type(t))
+def print_lyrics():
+    print ("I'm a lumberjack, and I'm okay.")
+    print ("I sleep all night and I work all day.")
+print_lyrics()
