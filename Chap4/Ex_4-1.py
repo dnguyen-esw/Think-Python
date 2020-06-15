067761d226e83994fd5d143aa649168c4b70d0da
ng-hoangduc <hoangduc104@gmail.com>
2020-06-15 22:41:30 +0700

    -A

---
+++ Chap4/Ex_4-1.py
@@ -0,0 +1,50 @@
+from swampy.TurtleWorld import *
+from math import *
+
+def polyline(t, n, length, angle):
+    """Draws n line segments with the given length and
+    angle (in degrees) between them. t is a turtle.
+    """
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
+def polygon(t, n, length):
+    """ Draws a polygon n sides with the given length.
+    t is a turtle.
+    """
+    angle=360.0/n
+    polyline(t, n, length, angle)
+def arc(t,r,angle):
+    """Draws an arc with the given radius and angle.
+
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2*pi*r*angle/360
+    n = int(arc_length/3)+1
+    step_length=arc_length/n
+    step_angle=float(angle)/n
+    polyline(t, n, step_length, step_angle)
+def circle(t, r):
+    """Draws a circle with the given radius.
+
+    t: Turtle
+    r: radius
+    """
+    arc(t, r, 360)
+if __name__ == '__main__':
+    world = TurtleWorld()    
+
+    bob = Turtle()
+    bob.delay = 0.001
+
+    # draw a circle centered on the origin
+    radius = 100
+    pu(bob)
+    fd(bob, radius)
+    lt(bob)
+    pd(bob)
+    circle(bob, radius)
+
+    wait_for_user()
