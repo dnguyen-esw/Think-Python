fb9c2fe6f64f1b22765998331893ddd7f506efff
ng-hoangduc <hoangduc104@gmail.com>
2020-06-18 20:00:27 +0700

    -A

---
+++ Chap5/Ex_5_5.py
@@ -0,0 +1,20 @@
+from swampy.TurtleWorld import *
+
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    fd(t, length*n)
+    lt(t, angle)
+    draw(t, length, n-1)
+    rt(t, 2*angle)
+    draw(t, length, n-1)
+    lt(t, angle)
+    bk(t, length*n)
+
+draw(bob,20,5)
+wait_for_user()
