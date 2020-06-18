584d16dee3db8a0c250ae186d651f2f8feac5c06
ng-hoangduc <hoangduc104@gmail.com>
2020-06-18 23:36:02 +0700

    -A

---
+++ Chap6/Ex_6_1.py
@@ -0,0 +1,11 @@
+def compare(x,y):
+    if x>y:
+        return 1
+    elif x==y:
+        return 0
+    elif x<y:
+        return -1
+
+if __name__ == "__main__":
+    compare(5,5)
+    
---
+++ Chap6/Ex_6_2.py
@@ -0,0 +1,7 @@
+from math import *
+
+def hypotenuse(a, b):
+    c=sqrt(a**2+b**2)
+    return c
+if __name__ == "__main__":
+    print(hypotenuse(3, 4))
