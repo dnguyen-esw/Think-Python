05c0c77f53e1eb118c1a691fc7796c626a074171
ng-hoangduc <hoangduc104@gmail.com>
2020-06-19 23:48:12 +0700

    -A

--- Chap6/Ex_6_3.py
+++ Chap6/Ex_6_3.py
@@ -4,6 +4,7 @@ def is_between(x, y, z):
         return True
     else:
         return False
+
 if __name__ == "__main__":
     pass
 
@@ -11,3 +12,5 @@ if is_between(5, 3, 4):
     print("True")
 elif ~is_between(2, 3, 1):
     print("False")
+
+
