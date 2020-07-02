5cd802a7ad8470f6b43f68f47ca5d1ec8876276e
ng-hoangduc <hoangduc104@gmail.com>
2020-07-03 01:39:34 +0700

    -A

---
+++ Chap9/Ex_9_2.py
@@ -0,0 +1,19 @@
+def has_no_e(file):
+    total=0
+    count=0
+    for line in file:
+        word=line.strip()
+        total+=1
+        if isinstance(word, str):
+            word=str(word)
+
+        if word.find('e')==-1:
+            print(word)
+            count+=1
+            
+    percentage=(count/total)*100
+    print(percentage)
+
+if __name__ == "__main__":
+    fin=open('words.txt')
+    has_no_e(fin)
